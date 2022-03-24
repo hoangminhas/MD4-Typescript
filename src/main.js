3025f2094b78421c1536a07f17eb377f0c24307d
hoangminhas <hoangminh9973@gmail.com>
2022-03-24 11:15:19 +0700

    thuc hanh cau dieu kien va loop

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-var width = 10;
-var height = 20.5;
-var area = width * height;
-console.log("Dien tich hcn la: ".concat(area));
+var sum = 0;
+var count = 0;
+for (var i = 2; count < 3; i++) {
+    var isPrime = true;
+    if (i == 2) {
+        sum += i;
+        count++;
+        continue;
+    }
+    for (var j = 2; j < i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (!isPrime) {
+        continue;
+    }
+    sum += i;
+    count++;
+}
+console.log(sum);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-let width: number = 10;
-let height: number = 20.5;
-let area: number = width*height;
-console.log(`Dien tich hcn la: ${area}`);
+let sum: number = 0;
+let count: number = 0;
+for (let i=2; count<3; i++) {
+    let isPrime: boolean = true;
+    if (i == 2) {
+        sum += i;
+        count++;
+        continue;
+    }
+
+    for (let j=2; j < i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (!isPrime) {
+        continue;
+    }
+    sum += i;
+    count++;
+}
+console.log(sum);
