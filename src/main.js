05df828eed3dd9b23ada81de16890baf7419790b
hoangminhas <hoangminh9973@gmail.com>
2022-03-24 10:49:19 +0700

    update 1

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var width = 10;
+var height = 20.5;
+var area = width * height;
+console.log("Dien tich hcn la: ".concat(area));
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+let width: number = 10;
+let height: number = 20.5;
+let area: number = width*height;
+console.log(`Dien tich hcn la: ${area}`);
