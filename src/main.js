e142d2308b7194ffd19696ee805ec132e118eff5
hoangminhas <hoangminh9973@gmail.com>
2022-03-24 11:27:48 +0700

    check prim and sum

--- src/main.js
+++ src/main.js
@@ -1,22 +1,51 @@
-var sum = 0;
-var count = 0;
-for (var i = 2; count < 3; i++) {
+// let sum: number = 0;
+// let count: number = 0;
+// for (let i=2; count<3; i++) {
+//     let isPrime: boolean = true;
+//     if (i == 2) {
+//         sum += i;
+//         count++;
+//         continue;
+//     }
+//
+//     for (let j=2; j < i; j++) {
+//         if (i % j == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//
+//     if (!isPrime) {
+//         continue;
+//     }
+//     sum += i;
+//     count++;
+// }
+// console.log(sum);
+function isPrime(num) {
     var isPrime = true;
-    if (i == 2) {
-        sum += i;
-        count++;
-        continue;
+    if (num < 2) {
+        isPrime = false;
     }
-    for (var j = 2; j < i; j++) {
-        if (i % j == 0) {
-            isPrime = false;
-            break;
+    else if (num > 2) {
+        for (var i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
         }
     }
-    if (!isPrime) {
-        continue;
-    }
-    sum += i;
-    count++;
+    return isPrime;
 }
-console.log(sum);
+function sumOfPrimes(arr) {
+    var sum = 0;
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var number = arr_1[_i];
+        if (isPrime(number)) {
+            sum += number;
+        }
+    }
+    return "Sum of primes:   + ".concat(sum);
+}
+console.log(isPrime(3));
+console.log(sumOfPrimes([2, 3, 5, 4, 7, 1]));
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,52 @@
-let sum: number = 0;
-let count: number = 0;
-for (let i=2; count<3; i++) {
-    let isPrime: boolean = true;
-    if (i == 2) {
-        sum += i;
-        count++;
-        continue;
-    }
+// let sum: number = 0;
+// let count: number = 0;
+// for (let i=2; count<3; i++) {
+//     let isPrime: boolean = true;
+//     if (i == 2) {
+//         sum += i;
+//         count++;
+//         continue;
+//     }
+//
+//     for (let j=2; j < i; j++) {
+//         if (i % j == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//
+//     if (!isPrime) {
+//         continue;
+//     }
+//     sum += i;
+//     count++;
+// }
+// console.log(sum);
 
-    for (let j=2; j < i; j++) {
-        if (i % j == 0) {
-            isPrime = false;
-            break;
+function isPrime(num: number) : boolean {
+    let isPrime = true;
+    if (num < 2) {
+        isPrime = false;
+    } else if (num>2) {
+        for (let i=2; i<= Math.sqrt(num); i++) {
+            if (num%i==0) {
+                isPrime = false;
+                break;
+            }
         }
     }
-
-    if (!isPrime) {
-        continue;
-    }
-    sum += i;
-    count++;
+    return isPrime;
 }
-console.log(sum);
+
+function sumOfPrimes(arr: Array<number>) : any {
+    let sum = 0;
+    for (let number of arr) {
+        if (isPrime(number)) {
+            sum += number;
+        }
+    }
+    return `Sum of primes = ${sum}`;
+}
+
+console.log(isPrime(3));
+console.log(sumOfPrimes([2,3,5,4,7,1]));
