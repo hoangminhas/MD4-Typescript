68f98d0001173d3b06b32b44bc36e8d06eded677
hoangminhas <hoangminh9973@gmail.com>
2022-03-24 12:07:33 +0700

    shape

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rectangle_1 = require("./rectangle");
+let hcn = new rectangle_1.Rectangle(5, 7, 2, 3);
+console.log(hcn.area());
+console.log(hcn.toString());
+//# sourceMappingURL=main.js.map
